59c27d481a34a7f3ae906aca055b1b6cb7f323c9
yanndav <yann.collindavid@gmail.com>
2021-05-17 18:16:24 +0200

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ecolometricsBack
+
+Please launch the server to access database on local host:
+1. Create virutal environment
+_python3 -m venv "api"_
+2. launch venv
+_source api bin/activate_
+3. Install packages
+pip install -r requirements.txr
+4. Launch app: 
+_flask run_
---
+++ wsgi.py
@@ -0,0 +1,27 @@
+import flask
+from flask import request, jsonify
+import os
+import json
+import re
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+dirPath = os.getcwd()
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+with open(os.path.join(dirPath,"testData","database.json"),'r') as base:
+    data = json.load(base)
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Just an ugly API</h1>
+<p>Things will be improved shortly.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/all', methods=['GET'])
+def api_all():
+    return jsonify(data)
+
+app.run()
