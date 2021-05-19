16c83e5cbeb55d80ca66eab604db3a1d571ebbfa
yanndav <yann.collindavid@gmail.com>
2021-05-19 22:49:51 +0200

    v0: api

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,8 +1,11 @@
 import collections
+from dns.rdatatype import NULL
 from flask import (Blueprint,
                     request,
                     jsonify)
 from app import mongo
+import json
+from bson import json_util
 
 api = Blueprint('api',__name__)
 
@@ -10,10 +13,35 @@ api = Blueprint('api',__name__)
 def home():
     results = mongo.db.list_collection_names()
 
-    return(jsonify(status = 'connected', collections=results))
+    return(jsonify(status = 'connected_to_api', collections=results))
 
-@api.route('/filter', methods=['GET'])
+@api.route('/getData', methods=['GET'])
 def api_filter():
+    # Retrieving user's request
     query_parameters = request.args.to_dict()
 
-    return query_parameters
+    # Dimensions:
+    dim = {'variable':1,'location':1,'values':1,'_id':0}
+
+    # Getting user's request parameters
+    # Collection 
+    col = query_parameters['collection']
+    # If searching for variable
+    search = {}
+    if 'variable' in query_parameters.keys():
+        var = query_parameters['variable']
+        search['variable'] = {"$gte":var}
+    
+    if 'location' in query_parameters.keys():
+        loc = query_parameters['location']
+        search['location'] = {"$gte":loc}
+
+    results = list(mongo.db[col].find(search, dim))
+    return json.dumps(results, default=json_util.default)
+    # return jsonify(search)
+
+@api.route('/getVariable', methods=['GET'])
+def api_variables():
+    # Retrieving user's request
+    query_parameters = request.args.to_dict()
+
