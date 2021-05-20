f5508579f530bcc83be1ddb751f991c8cbef3449
yanndav <yann.collindavid@gmail.com>
2021-05-20 14:35:06 +0200

    fix: secret key

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 class Config():
     load_dotenv()
-    # SECRET_KEY = environ.get("SECRET_KEY")
+    SECRET_KEY = environ.get("SECRET_KEY")
     DEBUG = environ.get("DEBUG")
     ENV = environ.get("FLASK_ENV")
     # SERVER_NAME = environ.get("SERVER_NAME")
