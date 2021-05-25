441410efde314ca064b5016ce6d204fd1552ccff
yanndav <yann.collindavid@gmail.com>
2021-05-25 08:32:36 +0200

    fix:cors header

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,19 @@ from flask import Flask
 from app.config import Config
 from flask_pymongo import PyMongo
 from flask_mail import Mail
-
+from flask_cors import CORS
 
 # Configuration
 mongo = PyMongo()
 mail = Mail()
+cors =CORS(resources='/getData/*')
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     mongo.init_app(app)
     mail.init_app(app)
+    cors.init_app(app)
 
     from app.api.routes import api
     app.register_blueprint(api)
